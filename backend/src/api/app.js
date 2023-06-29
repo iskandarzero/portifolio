6c846dd6e1ada7c60453218b319f451bd4bfea20
iskandarzero <riderzerotg@gmail.com>
2023-06-29 02:15:59 -0300

    feat: error middleware

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+require('express-async-errors');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+app.use((err, _req, res, _next) =>
+  res.status(err.code || 500).json({ error: `${err.message}` })
+);
 
 app.get('/', (_req, res) => {
   res.status(200).json({ok: true})
