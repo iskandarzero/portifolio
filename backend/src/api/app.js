d484bba1a9c69a427879b141c308c42c2f6d83cb
iskandarzero <riderzerotg@gmail.com>
2023-06-29 17:15:15 -0300

    feat: configure project route

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
+const projectRoute = require('../routes/projectRoute');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
+app.use('/', projectRoute)
+
 app.use((err, _req, res, _next) =>
   res.status(err.code || 500).json({ error: `${err.message}` })
 );
