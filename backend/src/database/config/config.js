04827784c17af1926d9fc2339936321bea62a4c3
iskandarzero <riderzerotg@gmail.com>
2023-06-28 21:56:05 -0300

    feat: sequelize init

---
+++ backend/src/database/config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const suffix = {
+  prod: "",
+  production: "",
+  dev: "-dev",
+  development: "-dev",
+  test: "-test",
+};
+
+const options = {
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  database: process.env.PGDATABASE,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  dialect: "postgres",
+  define: {
+    timestamps: true,
+  }
+};
+
+module.exports = {
+  development: {
+    ...options,
+  },
+  test: {
+    ...options,
+  },
+  production: {
+    ...options,
+  },
+};
