2f47ddf64f21f5d2c29f575b1e9de4cc3078d517
iskandarzero <riderzerotg@gmail.com>
2023-06-29 23:53:17 -0300

    feat: Navbar component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
-import { Inter } from 'next/font/google'
+import { DM_Mono } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
+
+import Navbar from '@/components/Navbar'
+
+const dmMono = DM_Mono({ weight: '400', subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={dmMono.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import logo from "../../public/images/logo.png"
+
+function Navbar() {
+  return (
+    <nav className="flex w-full bg-nav h-[65px] items-center justify-around fixed">
+      <Image src={logo} alt='logo' width={40} />
+      <div className="flex w-[25rem] text-white justify-between">
+        <a className="hover:text-yel" href="/#home">
+          Início
+        </a>
+        <a className="hover:text-yel" href="/#about">
+          Sobre
+        </a>
+        <a className="hover:text-yel" href="/#projects">
+          Projetos
+        </a>
+        <a className="hover:text-yel" href="/#contact">
+          Contato
+        </a>
+      </div>
+      <div className="flex w-[40px]"></div>
+    </nav>
+  )
+}
+
+export default Navbar
