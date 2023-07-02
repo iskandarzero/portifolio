38d1361f5b293077377a0fb3bc0e075b319d0977
iskandarzero <riderzerotg@gmail.com>
2023-07-02 02:55:27 -0300

    style: change navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from "../../public/images/logo.png"
 
 function Navbar() {
   return (
-    <nav className="flex w-full bg-nav h-[65px] items-center justify-around fixed">
+    <nav className="flex w-full bg-nav h-[65px] items-center justify-around fixed z-40">
       <Image src={logo} alt='logo' width={40} />
       <div className="flex w-[25rem] text-white justify-between">
         <a className="hover:text-yel" href="/#home">
