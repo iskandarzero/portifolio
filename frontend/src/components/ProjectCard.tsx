50086b061e65045ab507961ef4fc797bc96e4988
iskandarzero <riderzerotg@gmail.com>
2023-07-17 05:39:40 -0300

    feat: Repository component responsive

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -7,9 +7,9 @@ export default function ProjectCard({project}:{project:Project}) {
   const style = {color: "#D7B400", fontSize: 30};
 
   return (
-    <div className="group group/item p-[2px] pt-[6px] bg-yel relative mx-[1vw]">
+    <div className="group group/item p-[2px] pt-[6px] bg-yel relative mx-[5px] mb-[1vw]">
       <p className="ml-1">{project.name}</p>
-      <Image className="group/edit group-hover:brightness-50" src={project.image} alt="test" height={290} width={290} unoptimized={true} />
+      <Image className="group/edit group-hover:brightness-50" src={project.image} alt="test" height={240} width={240} unoptimized={true} />
       <div className="flex absolute top-[50%] left-[50%] transform translate-x-[-50%]">
         <Link href={project.github} target="_blank">
           <AiOutlineGithub className="group/edit invisible group-hover/item:visible hover:fill-black active:fill-white" style={style} />
