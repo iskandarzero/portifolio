fe09fea9b65bd66c05d1b02548083a99f464e914
iskandarzero <riderzerotg@gmail.com>
2023-07-18 06:31:54 -0300

    refactor: change position

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -5,9 +5,9 @@ function HeroSection() {
   return (
     <div id="home" className="flex justify-center bg-hero h-screen">
       <Image className="mt-[60px] object-cover md:object-scale-down" src={banner} alt="banner" />
-      <h1 className="absolute top-[31%] left-[50%] md:top-[16.5vw] md:left-[25vw] transform translate-x-[-50%] bg-black
+      <h1 className="absolute top-[34%] left-[50%] md:top-[16.5vw] md:left-[25vw] transform translate-x-[-50%] bg-black
         p-[0.5vw] text-yel text-2xl border-t-[26px]
-        border-[2px] border-yel h-fit md:w-fit w-[326px]"
+        border-[2px] border-yel h-fit md:w-fit w-[326px] shadow-2xl"
       >
         Isaac Farias <br/> Desenvolvedor Back-end
       </h1>
--- frontend/src/components/Repository.tsx
+++ frontend/src/components/Repository.tsx
@@ -29,14 +29,14 @@ export default function Repository() {
 
   return (
     <div id="projects" className="bg-hero h-screen flex items-center justify-center flex-col">
-      <h1 className="text-white text-3xl my-[30px] md:mt-0">Projetos</h1>
+      <h1 className="text-white text-3xl my-[30px] md:mt-[10px]">Projetos</h1>
       <div className="mb-[30px]">
         <button className="md:mx-2.5 mx-1 p-2 bg-yel hover:bg-yellow-400 active:bg-yellow-500" onClick={() => changeStatus('all')}>Todos</button>
         <button className="md:mx-2.5 mx-1 p-2 bg-yel hover:bg-yellow-400 active:bg-yellow-500" onClick={() => changeStatus('front-end')}>Front-end</button>
         <button className="md:mx-2.5 mx-1 p-2 bg-yel hover:bg-yellow-400 active:bg-yellow-500" onClick={() => changeStatus('back-end')}>Back-end</button>
         <button className="md:mx-2.5 mx-1 p-2 bg-yel hover:bg-yellow-400 active:bg-yellow-500" onClick={() => changeStatus('full stack')}>Full Stack</button>
       </div>
-      <div className="flex flex-col md:flex-row justify-center w-[70vw] flex-wrap overflow-auto">
+      <div className="flex flex-col md:flex-row justify-center w-[70vw] flex-wrap overflow-auto mb-[30px]">
         {filteredProjects.length > 0 ?
         filteredProjects.map((projectData, i) => (
           <ProjectCard project={projectData} key={i} />
