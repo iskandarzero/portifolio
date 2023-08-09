bdd77969d82603a655b94210f12911a6fde9391b
iskandarzero <riderzerotg@gmail.com>
2023-08-09 10:08:42 -0300

    refactor: Componentize Contact

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,32 +1,32 @@
+"use client"
+
 import Image from 'next/image'
 import contact from '../../public/images/contact.png'
 import { FaGithub, FaLinkedin, FaPhoneSquareAlt } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
+import FormContact from './FormContact';
+import Link from 'next/link'
 
 export default function Contact() {
   return (
     <div id="contact" className="flex items-center bg-abt h-screen relative">
-      <Image src={contact} alt="contact image" className="m-[1vw] h-[44vw] w-auto absolute invisible md:visible" />
-      <div className="flex flex-col-reverse md:flex-row bg-yel w-full md:max-h-[60%] max-h-[80%] md:justify-end py-[1vw]">
-        <div className="flex md:flex-col flex-wrap justify-evenly md:border-r-[1px] border-black mr-[1vw] mb-[10vw] md:mb-0">
-          <p className="mr-[1vw] flex items-center text-xs md:text-base"><FaGithub size={25} className="mr-[0.1vw]"/>GitHub</p>
-          <p className="mr-[1vw] flex items-center text-xs md:text-base"><FaLinkedin size={25} className="mr-[0.1vw]"/>LinkedIn</p>
-          <p className="mr-[1vw] flex items-center text-xs md:text-base"><MdEmail size={25} className="mr-[0.1vw]"/>riderzerotg@gmail</p>
-          <p className="mr-[1vw] flex items-center text-xs md:text-base"><FaPhoneSquareAlt size={25} className="mr-[0.1vw]"/>44 99716-8668</p>
+      <Image src={contact} alt="Imagem da seção de contatos" className="m-[1vw] h-[44vw] w-auto absolute invisible md:visible" />
+      <div className="flex flex-col-reverse md:flex-row bg-yel w-full md:max-h-[60%] max-h-full md:justify-end py-[1vw]">
+        <div className="flex self-center md:self-auto md:items-start flex-col flex-wrap justify-evenly md:border-r-[1px] border-black mr-[1vw] mb-[10vw] md:mb-0">
+          <Link href="https://github.com/iskandarzero" target="_blank">
+            <p className="mr-[1vw] flex items-center md:text-base"><FaGithub size={25} className="mr-[0.1vw]"/>GitHub</p>
+          </Link>
+          <Link href="https://www.linkedin.com/in/brtfarias/" target="_blank">
+            <p className="mr-[1vw] flex items-center md:text-base"><FaLinkedin size={25} className="mr-[0.1vw]"/>LinkedIn</p>
+          </Link>
+          <Link href="mailto:riderzerotg@gmail.com" target="_blank">
+            <p className="mr-[1vw] flex items-center md:text-base"><MdEmail size={25} className="mr-[0.1vw]"/>riderzerotg@gmail.com</p>
+          </Link>
+          <Link href="tel:+5544997168668" target="_blank">
+            <p className="mr-[1vw] flex items-center md:text-base"><FaPhoneSquareAlt size={25} className="mr-[0.1vw]"/>44 99716-8668</p>
+          </Link>
         </div>
-        <form className="flex flex-col md:mr-[10%] md:m-0 mx-[7%] mt-[10vw] mb-[4vw]">
-          <h1 className="text-3xl text-center md:text-start">Contato</h1>
-          <label htmlFor="name">Nome:</label>
-          <input className="my-[0.3vw] border-b-[1px] border-black bg-transparent" name="name" type="text"/>
-
-          <label htmlFor="email">E-mail:</label>
-          <input className="my-[0.3vw] border-b-[1px] border-black bg-transparent" name="email" type="email"/>
-
-          <label htmlFor="message">Mensagem:</label>
-          <textarea name="message" className="my-[0.3vw] border-b-[1px] border-black bg-transparent resize-none" rows={3} cols={30}/>
-
-          <button type="submit" className="bg-nav p-[1vw] m-[10px] border-[1px] w-[35%] md:w-full rounded-sm text-white self-center">Enviar</button>
-        </form>
+        <FormContact />
       </div>
     </div>
   )
---
+++ frontend/src/components/FormContact.tsx
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+export default function FormContact() {
+  const form = useRef();
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const clearForm = () => {
+    setValues({name: "", email: "", message: ""});
+  }
+
+  const sendEmail = (e:any) => {
+    e.preventDefault();
+    console.log(form.current);
+    
+    if (values.name && values.email && values.message) {
+      emailjs.sendForm('service_5oxuwgi', 'template_hh35z6l', form.current, 'XCvDESgIxLmpqo66H')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      clearForm()
+    } else {
+      alert('Preencha todos os campos!')
+    }
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="flex flex-col md:mr-[10%] md:m-0 mx-[7%] mt-[10vw] mb-[4vw]">
+      <h1 className="text-3xl text-center md:text-start">Contato</h1>
+      <label htmlFor="name">Nome:</label>
+      <input
+        className="my-[0.3vw] border-b-[1px] border-black bg-transparent"
+        name="name"
+        type="text"
+        value={values.name} onChange={(e) => setValues({ ...values, name: e.target.value })}
+      />
+
+      <label htmlFor="email">E-mail:</label>
+      <input
+        className="my-[0.3vw] border-b-[1px] border-black bg-transparent"
+        name="email"
+        type="email"
+        value={values.email} onChange={(e) => setValues({ ...values, email: e.target.value })}
+      />
+
+      <label htmlFor="message">Mensagem:</label>
+      <textarea
+        className="my-[0.3vw] border-b-[1px] border-black bg-transparent resize-none"
+        name="message"
+        rows={3}
+        cols={30}
+        value={values.message} onChange={(e) => setValues({ ...values, message: e.target.value })}
+      />
+
+      <button
+        className="bg-nav p-[1vw] m-[10px] border-[1px] w-[35%] md:w-full rounded-sm text-white self-center"
+        type="submit"
+      >
+        Enviar
+      </button>
+    </form>
+  )
+}
