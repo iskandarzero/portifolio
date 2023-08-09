54ae2841070ac4721a73930ca399d9d7f954bed9
iskandarzero <riderzerotg@gmail.com>
2023-08-09 11:37:58 -0300

    refactor: seeder

--- backend/src/database/seeders/20230629011121-demo-project.js
+++ backend/src/database/seeders/20230629011121-demo-project.js
@@ -6,7 +6,7 @@ module.exports = {
         {
           id: 1,
           name: 'Trybe Futebol Clube',
-          image: 'http://localhost:3001/images/trybe-futebol-clube.gif',
+          image: 'https://portifolio-k6gn.onrender.com/images/trybe-futebol-clube.gif',
           url: 'https://tfc-vercel-mu.vercel.app/leaderboard',
           type: 'back-end',
           github: 'https://github.com/iskandarzero/trybe-futebol-clube',
@@ -14,7 +14,7 @@ module.exports = {
         {
           id: 2,
           name: 'Delivery App',
-          image: 'http://localhost:3001/images/delivery-app.gif',
+          image: 'https://portifolio-k6gn.onrender.com/images/delivery-app.gif',
           url: 'https://delivery-app-vercel.vercel.app/login',
           type: 'full stack',
           github: 'https://github.com/iskandarzero/delivery-app',
@@ -22,7 +22,7 @@ module.exports = {
         {
           id: 3,
           name: "Recipes App",
-          image: 'http://localhost:3001/images/recipes-app.gif',
+          image: 'https://portifolio-k6gn.onrender.com/images/recipes-app.gif',
           url: 'https://recipes-app-ochre-mu.vercel.app/',
           type: 'front-end',
           github: 'https://github.com/iskandarzero/recipes-app',
